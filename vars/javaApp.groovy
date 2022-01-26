3ccfa5c5e5dadebaffb582de6979c8687fcc268e
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2022-01-27 04:18:27 +0530

    Update javaApp.groovy

--- vars/javaApp.groovy
+++ vars/javaApp.groovy
@@ -46,7 +46,7 @@ def call(String repoUrl){
                 sshagent(['dev-server']) {
                     // some block
                     sh "ssh -o StrictHostKeyChecking=no ec2-user@172.31.34.100 docker rm -f ${env.JOB_NAME} || true"
-                    sh "ssh -o StrictHostKeyChecking=no ec2-user@172.31.34.100 docker run -p 9000:8080 -d --name ${env.JOB_NAME} gauravdocker1234/${env.JOB_NAME}:${env.BUILD_NUMBER}"
+                    sh "ssh -o StrictHostKeyChecking=no ec2-user@172.31.34.100 docker run -p 9001:8080 -d --name ${env.JOB_NAME} gauravdocker1234/${env.JOB_NAME}:${env.BUILD_NUMBER}"
             }
         }
         }
