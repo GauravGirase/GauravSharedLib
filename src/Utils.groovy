9e71075df5365b9b6727645d7a125497372c1393
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2022-01-27 10:04:15 +0530

    Update Utils.groovy

--- src/Utils.groovy
+++ src/Utils.groovy
@@ -1,5 +1,18 @@
 class Utils {
     
+     /**
+     * Read resource file
+     *
+     * @Example
+     * <pre>
+     *     String content = Utils.loadResource('src/resources/my-file.txt')
+     * </pre>
+     *
+     * @param  path        path to the resource file
+     * @return             the file content
+     * @throws IOException if failed to read the file
+     */
+    
     static String loadResource(String path) throws IOException {
         return new File(path).getText('UTF-8');
     }
