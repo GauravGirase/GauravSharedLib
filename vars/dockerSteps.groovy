5d8ab6df59f374b795b536d51c13afb67688fb21
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2022-01-27 06:11:29 +0530

    Update dockerSteps.groovy

--- vars/dockerSteps.groovy
+++ vars/dockerSteps.groovy
@@ -1,5 +1,18 @@
+def gitcheckout(String repoUrl){
+    git branch: 'main', url: '${repoUrl}'
+}
+
+
 def login(){
     withCredentials([string(credentialsId: 'docpwd', variable: 'dockerpass')]) {
                         sh "docker login -u gauravdocker1234 -p ${dockerpass}"
-                    }
+                   }
+}
+
+def push(){
+    sh "docker push gauravdocker1234/${env.JOB_NAME}:${env.BUILD_NUMBER}"
+}
+
+def removeimage(){
+    sh "docker rmi gauravdocker1234/${env.JOB_NAME}:${env.BUILD_NUMBER}"
 }
