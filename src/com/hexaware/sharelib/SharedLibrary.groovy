84eb9ff98a52137b581643ee7720a7c404e3c02d
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2022-01-28 16:52:08 +0530

    Update SharedLibrary.groovy

--- src/com/hexaware/sharelib/SharedLibrary.groovy
+++ src/com/hexaware/sharelib/SharedLibrary.groovy
@@ -10,7 +10,7 @@ public class SharedLibrary {
   public void startBuild(String name) {
     
     steps.stage('Checkout') {
-            steps.git --version
+            steps.git
         }
     
   }
