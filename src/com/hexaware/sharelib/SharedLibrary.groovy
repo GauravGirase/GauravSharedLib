2d5628ecfa54b5dd3500b454b5f8fd02078f93b8
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2022-01-28 17:44:10 +0530

    Update SharedLibrary.groovy

--- src/com/hexaware/sharelib/SharedLibrary.groovy
+++ src/com/hexaware/sharelib/SharedLibrary.groovy
@@ -8,10 +8,6 @@ public class SharedLibrary {
   }
   
   public void startBuild(String repoUrl) {
-      steps.sh '''now="$(date)"
-                    printf "Current date and time %s\n" "$now"
-                    now="$(date +'%d/%m/%Y')"
-                    printf "Current date in dd/mm/yyyy format %s\n" "$now"
-                    echo "Starting backup at $now, please wait..."'''
+      steps.git branch: 'main', url: '${repoUrl}'
   }
 }
