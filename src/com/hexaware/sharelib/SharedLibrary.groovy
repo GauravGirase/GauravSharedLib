9824d2871cf9a032dd2a863b08c5f1b28685571a
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2022-01-28 19:39:47 +0530

    Update SharedLibrary.groovy

--- src/com/hexaware/sharelib/SharedLibrary.groovy
+++ src/com/hexaware/sharelib/SharedLibrary.groovy
@@ -11,13 +11,10 @@ public class SharedLibrary {
   void cloneRepository(){
     
     this.steps.git(
-                url           : "https://${GITHUB_DOMAIN}/${this.repository}.git",
+                url           : "https://github.com/GauravGirase/springBootAppDemo.git",
                 branch        : "main'
             )
-  
-  
-  
-  
+    
   }
  
 }
